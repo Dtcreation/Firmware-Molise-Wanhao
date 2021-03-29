5a83d6c0f1702922d261c0db7f3466c80cf33d14
Dtcreation <dtcreation3d@icloud.com>
2021-03-29 11:51:33 +0200

    Debug S Curve

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1727,7 +1727,7 @@
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 1.1    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
-  #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
+  //#define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
 #endif
 
 // @section leveling
