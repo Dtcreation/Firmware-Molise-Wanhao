3611fc505a253b958767fbcf60d4f939f205ceba
Dtcreation 3D <dtcreation3D@icloud.com>
2021-04-25 08:47:19 +0200

    Adj Z home feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1880,7 +1880,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (12*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
