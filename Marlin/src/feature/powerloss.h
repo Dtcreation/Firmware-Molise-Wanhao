1eb497c1a98f7f29a25c063072ef576d782a82a4
Dtcreation69 <dtcreation3d@icloud.com>
2023-07-27 07:46:38 +0200

    Molise 4.1

--- Marlin/src/feature/powerloss.h
+++ Marlin/src/feature/powerloss.h
@@ -67,8 +67,8 @@ typedef struct {
   #if HAS_HOME_OFFSET
     xyz_pos_t home_offset;
   #endif
-  #if HAS_POSITION_SHIFT
-    xyz_pos_t position_shift;
+  #if HAS_WORKSPACE_OFFSET
+    xyz_pos_t workspace_offset;
   #endif
   #if HAS_MULTI_EXTRUDER
     uint8_t active_extruder;
@@ -113,7 +113,7 @@ typedef struct {
   millis_t print_job_elapsed;
 
   // Relative axis modes
-  uint8_t axis_relative;
+  relative_t axis_relative;
 
   // Misc. Marlin flags
   struct {
@@ -138,7 +138,7 @@ class PrintJobRecovery {
   public:
     static const char filename[5];
 
-    static SdFile file;
+    static MediaFile file;
     static job_recovery_info_t info;
 
     static uint8_t queue_index_r;     //!< Queue index of the active command
